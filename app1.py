e004518c7b1d2149ed16d41ff92ff1268d443131
akhiill540@gmail.com <akhiill540@gmail.com>
2023-07-16 10:27:26 +0530

    added app1.py

---
+++ app1.py
@@ -0,0 +1,24 @@
+from flask import Flask,request
+import hashlib
+from flask import jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient('')
+# db = client.
+db = client['megashop']
+collection = db['items']
+
+@app.route('/hello')
+def helloRoute():
+    return jsonify('Hello World')
+
+@app.route('/items', methods=['POST'])
+def postItemRoute():
+    item = request.json
+    collection.insert_one(item)
+    return jsonify({'message': 'Added Successfully'})    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)    
