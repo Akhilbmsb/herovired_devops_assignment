766435901c4f309a3ef09fbfd974fdba3cd2cea8
Akhilbmsb <54345937+Akhilbmsb@users.noreply.github.com>
2023-10-28 13:17:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ from flask import jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('')
+# client = MongoClient('')
 # db = client.
-db = client['megashop']
-collection = db['items']
+# db = client['megashop']
+# collection = db['items']
 
 @app.route('/hello')
 def helloRoute():
     return jsonify('Hello World')
 
-@app.route('/items', methods=['POST'])
-def postItemRoute():
-    item = request.json
-    collection.insert_one(item)
-    return jsonify({'message': 'Added Successfully'})    
+# @app.route('/items', methods=['POST'])
+# def postItemRoute():
+#     item = request.json
+#     collection.insert_one(item)
+#     return jsonify({'message': 'Added Successfully'})    
 
 
 if __name__ == '__main__':
